686050c81c838a507b9b5e360cb1116a7fdeca02
jorge_ramos <zramos.ivan@gmail.com>
2023-07-20 22:20:37 -0600

    AWS changes

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const app = require("./src/server");
 const { DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 const databaseUrl = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`;
-const port = process.env.PORT || 8080;
+const port2 = process.env.PORT || 8080;
 
 mongoose
   .connect(databaseUrl)
   .then(() => {
     console.log("Conexión a la base de datos exitosa");
-    app.listen(port, () => {
+    app.listen(port2, () => {
       console.log("Nuestra api de clean arquitecture está prendida!");
     });
     // Levantar servidor
